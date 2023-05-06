777a0e6a8111f6401ab5510d7df826b8fd624e20
SEG <86530914+GitterTurtle77@users.noreply.github.com>
2023-05-06 19:55:14 +0100

    Delete ferb directory

--- ferb/script.js
+++
@@ -1,3 +0,0 @@
-fetch('https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=2358230&count=5&format=json&key=977C4BAFD60F47D399E240291A2E95B9', {method: 'GET', mode: 'cors'})
-    .then(response => response.json)
-    .then(data => console.log(data));
