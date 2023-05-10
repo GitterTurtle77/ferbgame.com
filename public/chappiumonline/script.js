6b1d90fce3fe59cde8de2bec833d40792041b2ee
Sean <seangrace5@gmail.com>
2023-05-10 15:01:13 +0100

    modified:   public/chappiumonline/index.html 	modified:   public/chappiumonline/script.js

--- public/chappiumonline/script.js
+++ public/chappiumonline/script.js
@@ -17,32 +17,34 @@ const database = firebase.database();
 
 var run = false;
 
-var isIos;
-if (
-  navigator.userAgent.match(/iPhone/i) ||
-  navigator.userAgent.match(/iPad/i) ||
-  navigator.userAgent.match(/iPod/i)
-) {
-  if ("standalone" in window.navigator && window.navigator.standalone) {
-    isIos = false;
-  } else {
-    isIos = true
-  }
-} else {
-  isIos = false;
-}
+// var isIos;
+// if (
+//   navigator.userAgent.match(/iPhone/i) ||
+//   navigator.userAgent.match(/iPad/i) ||
+//   navigator.userAgent.match(/iPod/i)
+// ) {
+//   if ("standalone" in window.navigator && window.navigator.standalone) {
+//     isIos = false;
+//   } else {
+//     isIos = true
+//   }
+// } else {
+//   isIos = false;
+// }
 
-var isAndroid = false;
+// var isAndroid = false;
 
-if (isIos) {
-    document.getElementById("iOS").style.display = "block";
-} else if (isAndroid) {
-    document.getElementById("Android").style.display = "block";
-}
-onAuthStateChanged(auth, (user) => {
+// if (isIos) {
+//     document.getElementById("iOS").style.display = "block";
+// } else if (isAndroid) {
+//     document.getElementById("Android").style.display = "block";
+// }
+
+firebase.auth().onAuthStateChanged((user) => {
+  console.log(user)
       if (user) {
         location.href = "/chappiumonline/home";
       } else {
         document.getElementById("Account").style.display = "block";
       }
-}
+});
