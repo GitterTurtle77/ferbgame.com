cd518681408458d8b6c6a9780ce190129520a2a7
Sean <seangrace5@gmail.com>
2023-05-13 20:16:35 +0100

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const Dotenv = require('dotenv-webpack');
-
 module.exports = {
   plugins: [
-    new Dotenv()
-  ]
-  }
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      // ...
+    }),
+  ],
 };
