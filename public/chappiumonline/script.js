c8c8e3bc09c22adbaea69b40de7b17718a53c973
SEG <86530914+GitterTurtle77@users.noreply.github.com>
2023-06-11 16:05:28 +0100

    Update script.js

--- public/chappiumonline/script.js
+++ public/chappiumonline/script.js
@@ -4,7 +4,7 @@ var uid = firebase.auth().currentUser.uid;
 // Create a reference to this user's specific status node.
 // This is where we will store data about being online/offline.
 var userStatusDatabaseRef = firebase.database().ref('/status/' + uid);
-const d = new Date();
+var d = new Date();
 
 // We'll create two constants which we will write to 
 // the Realtime database when this device is offline
@@ -20,7 +20,7 @@ var isOnlineForDatabase = {
 };
 
 // Create a reference to the special '.info/connected' path in 
-// Realtime Database. This path returns `true` when connected
+// Realtime Database. This path returns `true` when axconnected
 // and `false` when disconnected.
 firebase.database().ref('info/connected').on('value', function(snapshot) {
     // If we're not currently connected, don't do anything.
@@ -31,7 +31,7 @@ firebase.database().ref('info/connected').on('value', function(snapshot) {
     // If we are currently connected, then use the 'onDisconnect()' 
     // method to add a set which will only trigger once this 
     // client has disconnected by closing the app, 
-    // losing internet, or any other means.
+    // losing internet, or any other means
     userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase).then(function() {
         // The promise returned from .onDisconnect().set() will
         // resolve as soon as the server acknowledges the onDisconnect() 
@@ -39,8 +39,8 @@ firebase.database().ref('info/connected').on('value', function(snapshot) {
         // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
 
         // We can now safely set ourselves as 'online' knowing that the
-        // server will mark us as offline once we lose connection.
-        d = new Date()
+        // server will mark us as offline once we lose connection
+        isOfflineForDatabase.last_changed = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " " + d.getHours() + ":" + String(d.getMinutes()).padStart(2, '0'),
         userStatusDatabaseRef.set(isOnlineForDatabase);
     });
 });
